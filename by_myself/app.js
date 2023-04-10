f657c91a5537e95fd87db0270bf53df9ad0f9cdc
amit1703 <83718363+amit1703@users.noreply.github.com>
2023-04-10 17:14:14 +0300

    1

---
+++ by_myself/app.js
@@ -0,0 +1,9 @@
+let nums;
+let num= ["zero" , "one" , "two" , "three" , "four" , "five" , "six" , "seven" , "eight " , "nine"] 
+for(let i = 0 ; i< num.length ; i++){
+nums[i] = document.querySelectorAll(num[i])
+}
+let res = document.querySelector('#seven')
+res.innerHTML("5")
+
+
