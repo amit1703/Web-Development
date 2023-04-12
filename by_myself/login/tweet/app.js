ba513530308bcf4173545099e325a37f72ab6179
amit1703 <amit.izhari@gmail.com>
2023-04-12 17:59:24 +0300

    submit a tweet by myself

---
+++ by_myself/login/tweet/app.js
@@ -0,0 +1,15 @@
+const form = document.querySelector('#formid');
+const btn = document.querySelector('button');
+const input = document.querySelector('#tweet');
+const ul = document.querySelector('#ulid')
+form.addEventListener('submit', function(e){
+
+e.preventDefault();
+let tweet = input.value;
+const li = document.createElement('LI');
+li.innerText = tweet;   
+ul.append(li);
+input.value = '';
+
+
+});
