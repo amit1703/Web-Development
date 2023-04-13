0508299be57ab372358a6cb3728d2478189786ed
amit1703 <83718363+amit1703@users.noreply.github.com>
2023-04-13 20:40:52 +0300

    c

--- ajax_api_reqs/Axios/app.js
+++ ajax_api_reqs/Axios/app.js
@@ -7,10 +7,12 @@
 //     console.log("ERROR! ", e);
 //   });
 
+const h1s = document.querySelector('h1');
 const getStarWarsPerson = async (id) => {
   try {
     const res = await axios.get(`https://swapi.dev/api/people/${id}/`);
     console.log(res.data);
+    h1s.innerHTML = res.data.name
   } catch (e) {
     console.log("ERROR", e);
   }
