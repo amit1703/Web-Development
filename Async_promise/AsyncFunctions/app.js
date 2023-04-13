7c23af014e255f207a77fe5b363b3f3cc370dbb1
amit1703 <83718363+amit1703@users.noreply.github.com>
2023-04-13 17:55:29 +0300

    ajax and api code

--- Async_promise/AsyncFunctions/app.js
+++ Async_promise/AsyncFunctions/app.js
@@ -3,15 +3,15 @@
 
 
 const sing = async () => {
-    throw "OH NO, PROBLEM!"
-    return 'LA LA LA LA'
+    throw "OH NO, PROBLEM!" //reject
+    return 'LA LA LA LA' // resolve
 }
 
 sing()
-    .then(data => {
+    .then(data => { // if resolved , do this
         console.log("PROMISE RESOLVED WITH:", data)
     })
-    .catch(err => {
+    .catch(err => { // if rejected, do this
         console.log("OH NO, PROMISE REJECTED!")
         console.log(err)
     })
@@ -19,20 +19,20 @@ sing()
 
 
 
-const login = async (username, password) => {
-    if (!username || !password) throw 'Missing Credentials'
-    if (password === 'corgifeetarecute') return 'WELCOME!'
+const login = async (username, password) => { // func get two vars and checking them
+    if (!username || !password) throw 'Missing Credentials' // if there are no user name and pass , throw rejected , missing creds
+    if (password === 'corgifeetarecute') return 'WELCOME!' // if pas correct , return resolved , welcome!
     throw 'Invalid Password'
 }
 
 login('todd', 'corgifeetarecute')
-    .then(msg => {
+    .then(msg => { // if resolve do this
         console.log("LOGGED IN!")
-        console.log(msg)
+        console.log(msg) // this is the msg after the return (WELCOME!)
     })
-    .catch(err => {
+    .catch(err => { // if rejected , do this 
         console.log("ERROR!")
-        console.log(err)
+        console.log(err) // this is the msg after the throw (Missing creds)
     })
 
 
@@ -40,10 +40,11 @@ login('todd', 'corgifeetarecute')
 
 
 
-const delayedColorChange = (color, delay) => {
+const delayedColorChange = (color, delay) => { // func gets two var, 
+    //delay abd color and then changes the screen color
     return new Promise((resolve, reject) => {
         setTimeout(() => {
-            document.body.style.backgroundColor = color;
+            document.body.style.backgroundColor = color; //changing the screen color
             resolve();
         }, delay)
     })
@@ -58,8 +59,10 @@ const delayedColorChange = (color, delay) => {
 //     .then(() => delayedColorChange('violet', 1000))
 
 
-async function rainbow() {
-    await delayedColorChange('red', 1000)
+async function rainbow() { // code lines will run only after the prev 
+    //line finished because of await
+    await delayedColorChange('red', 1000) //callibg delayedColorChange and then
+    // only nxt line will run
     await delayedColorChange('orange', 1000)
     await delayedColorChange('yellow', 1000)
     await delayedColorChange('green', 1000)
@@ -73,20 +76,21 @@ async function rainbow() {
 
 
 async function printRainbow() {
-    await rainbow();
+    await rainbow(); // after the whole func ends, print this , because await key
     console.log("END OF RAINBOW!")
 }
 
 printRainbow();
 
-const fakeRequest = (url) => {
+const fakeRequest = (url) => { // generate a number for delay time,
+    // if the number is above 2000 , throw reject , else return resolve
     return new Promise((resolve, reject) => {
-        const delay = Math.floor(Math.random() * (4500)) + 500;
+        const delay = Math.floor(Math.random() * (4500)) + 500; 
         setTimeout(() => {
             if (delay > 2000) {
-                reject('Connection Timeout :(')
+                reject('Connection Timeout :(') // reject msg
             } else {
-                resolve(`Here is your fake data from ${url}`)
+                resolve(`Here is your fake data from ${url}`) // resolve msg 
             }
         }, delay)
     })
@@ -94,14 +98,15 @@ const fakeRequest = (url) => {
 
 
 async function makeTwoRequests() {
-    try {
-        let data1 = await fakeRequest('/page1');
-        console.log(data1);
+    try { // for if we get rejected , the code will crash so we need to use try and catch
+        let data1 = await fakeRequest('/page1'); // because of await data1 will match
+        // the resolve and only after that it will print the info 
+        console.log(data1); // data1 is the resolve msg (Here is your fake data from  url )
         let data2 = await fakeRequest('/page2');
         console.log(data2);
     } catch (e) {
         console.log("CAUGHT AN ERROR!")
-        console.log("error is:", e)
+        console.log("error is:", e) // e is the throw rejection error(Connection Timeout :()
     }
 
 }
