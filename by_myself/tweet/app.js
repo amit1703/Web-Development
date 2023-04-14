49af19801d54470a0b880fef398346e9cd458ea6
amit1703 <83718363+amit1703@users.noreply.github.com>
2023-04-14 15:01:06 +0300

    s

--- by_myself/tweet/app.js
+++ by_myself/tweet/app.js
@@ -1,4 +1,3 @@
-const { default: axios } = require("axios");
 
 const form = document.querySelector('#formid');
 const btn = document.querySelector('button');
@@ -11,15 +10,8 @@ let tweet = input.value;
 const li = document.createElement('LI');
 li.innerText = tweet;   
 ul.append(li);
-input.value = pic();
+input.value = '';
 
 });
 
 
-const pic = async() =>{
-    
-    const jokes = await axios.get("https://icanhazdadjoke.com/")
-    return jokes.data.joke
-
-
-}
