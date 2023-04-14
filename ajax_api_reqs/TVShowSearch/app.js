9f77259517a9aca9c222fde1a98211a8b5e4944b
amit1703 <83718363+amit1703@users.noreply.github.com>
2023-04-14 03:51:34 +0300

    understanding of tvshow api req(herdest yet)

--- ajax_api_reqs/TVShowSearch/app.js
+++ ajax_api_reqs/TVShowSearch/app.js
@@ -1,15 +1,17 @@
 const form = document.querySelector('#searchForm');
 form.addEventListener('submit', async function (e) {
     e.preventDefault();
-    const searchTerm = form.elements.query.value;
+    const searchTerm = form.elements.query.value; //the input value (what the name of the movie)
     const config = { params: { q: searchTerm } }
-    const res = await axios.get(`http://api.tvmaze.com/search/shows`, config);
+    const res = await axios.get(`http://api.tvmaze.com/search/shows`, config); // what api we sending req
     makeImages(res.data)
-    form.elements.query.value = '';
+    form.elements.query.value = ''; // reseting the input text
 })
 
 const makeImages = (shows) => {
+//shows is a json content , that includs all the movies that the input word includs from the api
     for (let result of shows) {
+//it goes every item in the json and taking the pic of the item and making it img elemnt 
         if (result.show.image) {
             const img = document.createElement('IMG');
             img.src = result.show.image.medium;
