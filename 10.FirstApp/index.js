d1c8e8178bbc67c3766ab4df09e119e7c8094739
amit1703 <amit.izhari@gmail.com>
2023-05-07 16:35:03 +0300

    express basic understanding

--- 10.FirstApp/index.js
+++ 10.FirstApp/index.js
@@ -18,10 +18,38 @@ app.get('/dogs', (req,res) =>{
 app.get('/', (req , res ) =>{
     res.send('this is the home page')
 })
+
 //these are all post reqs , what usually in use. 
 
  app.listen(3000, ()=>{// this function will run only when the server is up ,
     // when the server is up , this code will run.
 
         console.log("listning for port 3000!")
+ })
+
+ //express path params!
+
+ app.get('/r/:subreddit', (req , res) =>{
+    // the :subreddit is a param ,
+    // if the path matches to /r/something run this function
+    const {subreddit} = req.params; // the subreddit info 
+    // /r/tahel , params = tahel 
+    res.send(`browsing the ${subreddit}subreddit`); 
+ 
+ }) 
+ app.get('/r/:subreddit/:postID', (req , res) =>{
+    // the :subreddit and :postId is a param ,
+    // if the path matches to /r/something run this function
+    const {subreddit , postID} = req.params; // the subreddit info 
+    // /r/tahel , params = tahel 
+    res.send(`browsing the ${subreddit} subreddit, post id ${postID}`); 
+ 
+ }) 
+
+ //quary strings
+ app.get('/search', (req , res) =>{
+    console.log(req.query)
+    const {q} = req.query;
+    res.send(`seach for: ${q}`)
+    
  })
