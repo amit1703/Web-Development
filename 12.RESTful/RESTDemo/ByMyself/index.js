8922acc89a4e2e1870ad882d4c2942b9ac359239
amit1703 <amit.izhari@gmail.com>
2023-06-05 02:24:15 +0300

    by myself
    finally the code worked by myself w/o any guides!

---
+++ 12.RESTful/RESTDemo/ByMyself/index.js
@@ -0,0 +1,44 @@
+const express = require('express')
+const app = express();
+
+
+app.set(express.json);//for parsing to post as a json
+app.set(express.urlencoded({extended:true}))// for parsing data to post (must)
+
+
+const comments = [
+    {
+        username:'amit',
+        text:'this is a bad video'
+    },
+    {
+        username:'ron',
+        text:'this is a good video'
+    },
+    {
+        username:'tahel',
+        text:'i hate jews'
+    },
+    {
+        username:'fakeuser',
+        text:'for only 5$ you get a new house!'
+    },
+
+]
+
+app.get('/comments' , (req,res)=>{
+    res.render('index.ejs', ({comments}))
+})
+app.get('/comments/new' , (req,res)=>{
+    res.render('newcomment.ejs' ,({comments}))
+})
+app.post('/comments' , (req,res)=>{
+    const { username, comment } = req.body;//extracting the data from the post req(the form data)
+    comments.push({ username, comment, id: uuid() })// adding the data to the db(to the object)
+    res.redirect('/comments');
+})
+
+app.listen(3000 , ()=>{
+    console.log('listining on port 3000!')
+
+})
