388a66e3eddb90991fc91350fdf467874ee1f7b2
amit1703 <amit.izhari@gmail.com>
2023-12-30 21:06:47 +0200

    added edit and delete
    פשוט לא להיות טיפש, לא לשכוח אותיות ולהסתכל טוב על מה שעשיתי לפני

--- AirBnB/app.js
+++ AirBnB/app.js
@@ -3,8 +3,8 @@ const app = express();
 const path = require('path')
 const mongoose = require('mongoose')
 const Apartment = require('./models/Apartment')
-const methodOverride = require('method-override')
 
+const methodOverride = require('method-override')
 app.use(methodOverride('_method'))
 
 mongoose.connect('mongodb://localhost:27017/AirBnB', {})//connection to mongoDB
@@ -21,50 +21,63 @@ app.set('view engine' , 'ejs')
 app.set('views', path.join(__dirname,'views'))
 app.use(express.urlencoded({extended: true}))
 // up to here, is a must 
+
+
+
+
+
 app.get('/' , (req,res)=>{
 
-    res.render('home.ejs')
+    res.render('home')
 })
 
 app.get('/apartments', async (req,res)=>{
 
     const apartments = await Apartment.find({})
-    res.render('Apartment/index.ejs' ,{apartments})
+    res.render('Apartment/index' ,{apartments})
 })
 
 app.get('/apartments/new',(req,res)=>{
     //sends to the new Apartment form
-    res.render('Apartment/new.ejs')
+    res.render('Apartment/new')
 }) 
+app.post('/apartments', async(req,res)=>{
+    //relate to new Apartment
+        const apartment = new Apartment(req.body)
+        console.log(apartment.title)
+        await apartment.save()
+        res.redirect(`/apartments/${apartment._id}`)
+    })
 
 app.get('/apartments/:id', async (req,res)=>{
     const { id } = req.params;
     const apartment = await Apartment.findById(id)
-    res.render('Apartment/show.ejs' ,{apartment})
+    res.render('Apartment/show' ,{apartment})
 })
 
 app.get('/apartments/:id/edit', async(req,res)=>{
     //relate to edit a Apartment
     const { id } = req.params;
     const apartment = await Apartment.findById(id)
-    res.render('Apartment/edit.ejs' ,{apartment})
+    res.render('Apartment/edit' ,{apartment})
 })
 
 app.put('/apartments/:id', async(req,res)=>{
     //updating the Apartment (error 30.12.23)
     const {id} = req.params;
-    const apartment = await Apartment.findByIdAndUpdate(id , {...req.body.apartment})
-
-    res.redirect('Apartment/show.ejs',{apartment})
-
+    const apartment = await Apartment.findByIdAndUpdate(id, {...req.body.apartment})
+    console.log(apartment.title)
+    res.redirect(`/apartments/${apartment._id}`)
 })
 
-app.post('/apartments', async(req,res)=>{
-//relate to new Apartment
-    const apartment = new Apartment(req.body.apartment)
-    await apartment.save()
-    const apartments = await Apartment.find({})
-    res.redirect('Apartment/index.ejs', {apartments})
+app.delete('/apartments/:id', async(req,res)=>{
+
+    const {id} = req.params;
+    await Apartment.findByIdAndDelete(id)
+
+    res.redirect('/apartments')
+
+
 })
 
 app.listen(3000, ()=>{
