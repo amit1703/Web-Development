f17f6acd1951a36e25642a860f51af5a05a4a96d
amit1703 <amit.izhari@gmail.com>
2024-01-14 17:56:53 +0200

    1

--- AirBnB/app.js
+++ AirBnB/app.js
@@ -1,11 +1,11 @@
-const express = require('express');
+const express = require('express');// server
 const app = express();
 const path = require('path')
-const mongoose = require('mongoose')
+const mongoose = require('mongoose')//db
 const Apartment = require('./models/Apartment')
 const ejsMate = require('ejs-mate')
-const Review = require('./models/Reviews')
-
+const AppError = require('./AppError')
+const joi  = require('joi')// serverside errors (not in use rn...)
 
 const methodOverride = require('method-override');
 const { render } = require('ejs');
@@ -29,7 +29,7 @@ app.use(express.urlencoded({extended: true}))
 // up to here, is a must 
 
 
-app.use((err, req ,res ,next)=>{
+app.use((err, req ,res ,next)=>{ // need checking if can be deleted 
     //when will be an express error(like route exist but there is an error there) then this function will run
     console.log('error ', err)
     next(err)
@@ -41,74 +41,118 @@ app.get('/' , (req,res)=>{
     res.render('home')
 })
 
-app.get('/apartments', async (req,res)=>{
-
+app.get('/apartments', async (req,res,next)=>{
+    // the next line is for the middleware to run (w.o the next line the middleware wont run)
+    try{
     const apartments = await Apartment.find({})
     res.render('Apartment/index' ,{apartments})
+    }catch(e){
+        //when the code cant be execute next(e) will run, the error middleware will work and send an msg and status code error for the specific error (e)
+        next(e)
+    }
 })
 
-app.get('/apartments/new',(req,res)=>{
+app.get('/apartments/new',(req,res,next)=>{
     //sends to the new Apartment form
+    try{
     res.render('Apartment/new')
+}catch(e){
+    next(e)
+}
 }) 
-app.post('/apartments', async(req,res)=>{
+app.post('/apartments',  async(req,res,next)=>{
     //relate to new Apartment
+    try{
         const apartment = new Apartment(req.body)
         console.log(apartment.title)
         await apartment.save()
         res.redirect(`/apartments/${apartment._id}`)
+    }
+    catch(e){
+        // if there is an error with adding a new apartment ,the middlware app.use(err) will run
+        return next(e)
+    }
     })
 
-app.get('/apartments/:id', async (req,res)=>{
+app.get('/apartments/:id', async (req,res,next)=>{
+    try{
+
+    
     const { id } = req.params;
     const apartment = await Apartment.findById(id)
+    if(!apartment){
+       return next(new AppError('Apartment not found', 404))// w/o the return the next line will run and then we will get ejs error because there is no such an apramtment with this id
+    }
     res.render('Apartment/show' ,{apartment})
+}
+catch(e){
+    next(e)
+}
 })
 
-app.get('/apartments/:id/edit', async(req,res)=>{
+app.get('/apartments/:id/edit', async(req,res,next)=>{
     //relate to edit a Apartment
+    try
+    {
     const { id } = req.params;
     const apartment = await Apartment.findById(id)
     res.render('Apartment/edit' ,{apartment})
+}catch(e){
+    next(e)
+}
 })
 
-app.put('/apartments/:id', async(req,res)=>{
-    //updating the Apartment (error 30.12.23)
-    const {id} = req.params;
+app.put('/apartments/:id', async(req,res,next)=>{
+try{
+      const {id} = req.params;
     const apartment = await Apartment.findByIdAndUpdate(id, {...req.body.apartment})
     console.log(apartment.title)
     res.redirect(`/apartments/${apartment._id}`)
+}catch(e){
+    next(e)
+}
 })
 
-app.delete('/apartments/:id', async(req,res)=>{
-
+app.delete('/apartments/:id', async(req,res,next)=>{
+try
+{
     const {id} = req.params;
     await Apartment.findByIdAndDelete(id)
 
     res.redirect('/apartments')
-
-
-})
-
-app.get('/apartments/:id/review', (req,res)=>{
-    const {id} = req.params;
-    res.render('Apartment/review.ejs',({id}))
+}catch(e){
+    next(e)
+}
 
 })
 
-app.post('/apartments/:id', async (req,res)=>{
- 
-    const {id} = req.params;
-    const { body, rating} = req.body.review;
-    const review = new Review({
-        body: body,
-        rating : rating
-    })
-    await review.save()
-   res.redirect(`/apartments/${id}`)
+app.all('*', (req,res,next)=>{
 
+    return next(new AppError('page not found',404))
+})
+
+app.use((err, req ,res , next)=>{
+
+    const { message = 'somthing went wrong', status = 500} = err;
+    res.status(status).render('error',{message, status})
 })
 
 app.listen(3000, ()=>{
     console.log('on port 3000')
 })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
